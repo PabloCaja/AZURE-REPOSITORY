30e3305bc9575369e9996f50550a0d797f1404b1
Pablo Caja Martínez <pablo.caja@outlook.es>
2022-12-01 19:53:15 +0100

    confi

--- 2. PYTHON/python01inicio.py
+++ 2. PYTHON/python01inicio.py
@@ -7,6 +7,6 @@ numero = 21
 # Los tipos de dato String irán entre comillas dobles
 texto = "Mi primer String en Python"
 # Debemos convertir el número a string para concatenar
-print("El numero es " + numero)
+print("El numero es " + str(numero))
 
 print("Mi texto es : " + texto)
