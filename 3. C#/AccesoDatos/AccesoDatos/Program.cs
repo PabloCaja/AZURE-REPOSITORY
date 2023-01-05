038e66398f86e93e619651e11e57f2cb6f14313d
Pablo Caja Martínez <pablo.caja@outlook.es>
2023-01-05 22:10:17 +0100

    05-01-2023

---
+++ 3. C#/AccesoDatos/AccesoDatos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AccesoDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
