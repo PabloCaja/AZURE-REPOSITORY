36ec1e8f32fab8e53c052b3e47f3b42e2fc409b1
Pablo Caja Martínez <pablo.caja@outlook.es>
2023-01-14 18:19:55 +0100

    14-01-2023

--- 3. C#/FUNDAMENTOS/MVC/PrimerMVC2023/PrimerMVC2023/Controllers/InformacionController.cs
+++ 3. C#/FUNDAMENTOS/MVC/PrimerMVC2023/PrimerMVC2023/Controllers/InformacionController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMVC2023.Models;
 
 namespace PrimerMVC2023.Controllers
 {
@@ -10,7 +11,51 @@ namespace PrimerMVC2023.Controllers
         }
         public IActionResult ControllerView()
         {
+            //ENVIAMOS INFORMACION SIMPLE
+            ViewBag.Nombre = "Alumno";
+            ViewData["Descripcion"]= "Estoy aprendiendo Net Core";
+            ViewData["YEAR"] = "2023";
             return View();
         }
+        public IActionResult ControllerViewModel()
+        {
+            //CREAMOS UN OBJETO PERSONA PARA ENVIARLO
+            Persona persona = new Persona();
+            persona.Nombre = "Alumno Net Core";
+            persona.Edad = 24;
+            persona.Email = "alumnonetcore@gmail.com";
+            return View(persona);
+        }
+        public IActionResult ControllerViewModelList()
+        {
+            List<Persona> personas = new List<Persona>();
+            for (int i= 1; i <= 10; i++)
+            {
+                Persona persona = new Persona();
+                persona.Nombre = "Persona " + i;
+                persona.Edad = i + 20;
+                persona.Email = "persona" + i + "@gmail.com";
+                personas.Add(persona);
+                persona.Imagen = "https://juegosdeconstruccion.net/wp-content/uploads/2021/12/juguetes-sets-lego-batman.jpg";
+            }
+            return View(personas);
+        }
+        public IActionResult ViewControllerGet(String nombre, int edad)
+        {
+            ViewData["MENSAJE"] = nombre + ", Edad: " + edad;
+            return View();
+        }
+        public IActionResult ViewControllerPost()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ViewControllerPost(String cajanombre, int cajaedad)
+        {
+            ViewData["MENSAJE"] = "Nombre: " + cajanombre + ", Edad: "
+               + cajaedad;
+            return View();
+        }
+
     }
 }
