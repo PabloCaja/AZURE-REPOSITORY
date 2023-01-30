68afc258f888c75b875edbeb9485b20e12886d27
Pablo Caja Martínez <pablo.caja@outlook.es>
2023-01-30 04:44:49 +0100

    30-01-2023B

---
+++ 3. C#/FUNDAMENTOS/MVC/RolPrueba1/RolPrueba1/Models/Jugadores.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RolPrueba1.Models
+{
+    [Table("JUGADORES")]
+    public class Jugadores
+    {
+        [Key]
+        [Column("JUGADOR")]
+        public string Jugador { get; set; }
+
+    }
+}
